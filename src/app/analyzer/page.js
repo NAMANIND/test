89d4e0660a1eafbd6bad9f4f1a13eb7d800a1a84
Naman rai <namanrai309@gmail.com>
2024-11-02 22:29:39 +0530

    ok

--- src/app/analyzer/page.js
+++ src/app/analyzer/page.js
@@ -1,15 +1,12 @@
 "use client";
-import EnhancedFileUpload from "../../components/home/homepage";
+// import EnhancedFileUpload from "../../components/home/homepage";
 import Navbar from "../../components/home/navbar";
 
-
 export default function Analyzer() {
-  return(
-      <div>
-        {/* <Navbar/> */}
-        <EnhancedFileUpload/>
-      </div>
-    );
+  return (
+    <div>
+      {/* <Navbar/> */}
+      {/* <EnhancedFileUpload/> */}
+    </div>
+  );
 }
-
-
