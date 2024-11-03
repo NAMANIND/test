dd1faebe759dc3aa9175c8ac1ee53e41fbf617cd
Naman rai <namanrai309@gmail.com>
2024-11-03 11:52:56 +0530

    update

--- src/components/magnet/WorkSection.js
+++ src/components/magnet/WorkSection.js
@@ -178,7 +178,7 @@ const WorkSection = () => {
 
             {/* Title and type badge with improved layout */}
             <div className="flex items-center justify-between mb-6">
-              <h3 className="text-4xl font-extralight tracking-tight text-white font-Bitter">
+              <h3 className="md:text-4xl text-2xl font-extralight tracking-tight text-white font-Bitter">
                 {item.title}
               </h3>
 
